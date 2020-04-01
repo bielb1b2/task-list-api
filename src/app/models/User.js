ddfd6a55ff8ddde4adf497b74f4e29b18aa65776
bielb1b2 <gabriel.vitinho@hotmail.com>
2020-04-01 03:44:59 -0300

    Configurando banco

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,5 +1,7 @@
 const Sequelize = require('sequelize');
 const { Model } = require('sequelize');
+const bcrypt = require('bcryptjs');
+
 
 class User extends Model{
     static init(sequelize){
@@ -7,6 +9,7 @@ class User extends Model{
             {
                 name: Sequelize.STRING,
                 email: Sequelize.STRING,
+                password: Sequelize.VIRTUAL,
                 password_hash: Sequelize.STRING,
             },
 
@@ -14,6 +17,14 @@ class User extends Model{
                 sequelize,
             }
         );
+
+        this.addHook('beforeSave', async user => {
+            if(user.password){
+                user.password_hash = await bcrypt.hash(user.password, 8);
+
+            }
+        });
+        return this;
     }
 }
 
